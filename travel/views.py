5f222e39eadd9e37beb8958b5e769e1f2474955e
changchiajung <42213793+changchiajung@users.noreply.github.com>
2019-03-22 11:32:44 +0800

    新增sch頁面
    sch頁面稍微做ajax核後端:
    1. 目前先以 input 作為更改順序的方式
    2. 儲存方式有3種:
      2.1 離開頁面自動儲存
      2.2 每3分鐘自動儲存
      2.3 利用儲存按鈕手動儲存
    3. 傳回view之後更新db中的sequence
    
    TO DO:
    1. fontend的順序更改方式
    2. 新增景點應該需要傳回view 取得景點資料 並回傳fontend做顯示更先
    3. 承2 新增景點時需要做ajax

--- travel/views.py
+++ travel/views.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 import json
 # import requests
 from django.shortcuts import render
+from django.http import JsonResponse
 from .models import Site, Schedule, MapSite
 
 
@@ -29,7 +30,59 @@ def search_dis(request):
 
 
 def sch(request, id_num):
-    return render(request, 'sch.html')
+    if request.method == "GET":
+        content = {}
+        site_seq = []
+        site_seq_detail = []
+        show_schedule = Schedule.objects.get(id=id_num)
+        # get the Schedule by id
+        show_schedule.count += 1
+        # update browse count
+        show_schedule.save()
+        # save model
+        id_seq = [day.split(',') for day in show_schedule.sequence.split('&')]
+        # split sequence to each day (type => string)
+        for sequences in id_seq:
+            site_seq.append([Site.objects.get(id=id) for id in sequences])
+        # split each site in day (type => list [int])
+        for each_day in site_seq:
+            tem = []
+            # renew list
+            for each_site in each_day:
+                site_detail = each_site.site_Id
+                # get the corresponding object in mapsite
+                tem.append({
+                    'site_name': site_detail.site_name,
+                    'location_Id': site_detail.location_Id,
+                    'image': site_detail.image,
+                    'phone_number': site_detail.phone_number,
+                    'address': site_detail.address,
+                    'count': site_detail.count,
+                })
+                # put data in the dict
+            site_seq_detail.append(tem)
+            # in this way the data structure will be [ [ {} .,. {} ],[ {} ... {} ]...[]]
+        content['site_seq_detail'] = site_seq_detail
+        content['schedule_detail'] = {
+            'title': show_schedule.title,
+            'author': show_schedule.author,
+            'content': show_schedule.content,
+            'image': show_schedule.image,
+            'days': show_schedule.days,
+            'sequence': show_schedule.sequence,
+        }
+        # put the detail of Schedule in the content
+        return render(request, 'sch.html',content)
+    else:
+        message = request.POST.get('message', '')
+        print(message)
+        content={}
+        content["sequence"] = message
+        show_schedule = Schedule.objects.get(id=id_num)
+        show_schedule.sequence=message
+        show_schedule.save()
+        return JsonResponse(content)
+
 
 
 def show(request):
